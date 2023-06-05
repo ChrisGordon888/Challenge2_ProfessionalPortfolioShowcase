6b3712168e53d747dde3e7f105b7d3363c103215
Chris Gordon <chris.c.gordon888@gmail.com>
2023-06-05 06:15:32 -0500

    Updated HTML and CSS. I added an index.js file and began incorporating some new features.

---
+++ assets/javascript/index.js
@@ -0,0 +1,51 @@
+// Text fade
+$(document).ready(function() {
+    $(".about-text").hide().fadeIn(3333);
+});
+
+// Dark mode
+$(document).ready(function() {
+    $("#dark-mode-toggle").click(function() {
+        $("body").toggleClass("dark-mode");
+        if ($("body").hasClass("dark-mode")) {
+            $("#dark-mode-toggle").text("Light Mode");
+        } else {
+            $("#dark-mode-toggle").text("Dark Mode");
+        }
+    });
+});
+
+// Code for the smooth scroll function
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        let target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
+
+// Code for the scrolling header color change function
+$(document).ready(function() {
+    $(window).scroll(function() {
+        let scroll = $(window).scrollTop();
+        if (scroll > 300) {
+            $(".header-left").css("background", "#343a40");
+        } else {
+            $(".header-left").css("background", "transparent"); 
+        }
+    });
+});
+
+// Code for changing the banner content on page load
+$(document).ready(function() {
+    let bannerContent = ["Welcome to my Portfolio", "Christopher Gordon", "Web Developer", "Let's create something together"];
+    let i = 0;
+    setInterval(function() {
+        $("#banner-content").text(bannerContent[i]);
+        i = (i + 1) % bannerContent.length;
+    }, 2000);
+});
